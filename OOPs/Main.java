17f4e93c66f48f50dc3e22d6d221dc07ff9c7c21
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2025-08-05 15:37:36 +0530

    initial commit

---
+++ OOPs/Main.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+class Car{
+    private String carID;
+    private String brand;
+    private String model;
+    private double basePricePerDay;
+    private boolean isAvailable;
+
+    public Car(String carId, String brand, String model, double basePricePerDay) {
+        this.carID = carId;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+        this.isAvailable = true; // Cars are available by default
+    }
+
+    public String getCarID() {
+        return carID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double calculatePrice(int rentalDays) {
+        return basePricePerDay + rentalDays;
+    }
+
+    public void returnCar(){
+        isAvailable = true;
+    }
+}
+
+class Customer{
+    private String CustomerName;
+    private String CustomerID;
+
+    public Customer(String CustomerName, String CustomerID){
+        this.CustomerName = CustomerName;
+        this.CustomerID = CustomerID;    
+    }
+
+    public String getCustomerName(){
+        return CustomerName;
+    }
+
+    public String getCustomerID(){
+        return CustomerID;
+    }
+}
+
+class Rental{
+    private Car car;
+    private Customer customer;
+    private int rentalDays;
+
+    public Rental(Car car, Customer customer, int rentalDays){
+        this.car = car;
+        this.customer = customer;
+    }
+
+    public Car getCar(){
+        return car;
+    }
+
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    public int getRentalDays(){
+        return rentalDays;
+    }
+}
+
+class CarRentalSystem{
+    
+}
